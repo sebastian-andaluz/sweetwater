1976cb5fd3be7a952cd33c8b5f8cd6a4743277cb
sebastian-andaluz <sebastian.andaluz3@gmail.com>
2023-07-19 13:22:00 -0400

    Initial commit, task 1 worked on

---
+++ task1.php
@@ -0,0 +1,54 @@
+<?php
+    // Grab SQL File
+    $directory = __DIR__;
+    $file = $directory . '/sweetwater_test.sql';
+    $sweetwaterContents = file_get_contents($file);
+
+    // Regular expression pattern to match comments
+    $pattern = "/\(\d+,'(.*?)'/s";
+    preg_match_all($pattern, $sweetwaterContents, $matches);
+
+
+    $categories = array(
+        'Candy' => array(),
+        'Call Me / Don\'t Call Me' => array(),
+        'Referred' => array(),
+        'Signature Requirements' => array(),
+        'Miscellaneous' => array(),
+    );
+
+    $position = 0;
+
+    foreach ($matches[1] as $comment) {
+        // Decode special characters like  (PU2)
+        $comment = mb_convert_encoding($comment, 'ISO-8859-1', 'UTF-8');
+        
+        // Remove newlines and extra spaces from the comment
+        $comment = str_replace(array("\n", "\r"), ' ', $comment);
+        $comment = preg_replace('/\s+/', ' ', $comment);
+        
+        // echo "Extracted comment: $comment\n";
+        $comment = stripslashes($comment);
+
+        if (stripos($comment, 'candy') !== false) {
+            $categories['Candy'][] = $comment;
+        } elseif (stripos($comment, 'call me') !== false || stripos($comment, "don't call me") !== false) {
+            $categories['Call Me / Don\'t Call Me'][] = $comment;
+        } elseif (stripos($comment, 'referred') !== false) {
+            $categories['Referred'][] = $comment;
+        } elseif (stripos($comment, 'signature') !== false) {
+            $categories['Signature Requirements'][] = $comment;
+        } else {
+            $categories['Miscellaneous'][] = $comment;
+        }
+    }
+
+    // Display the categorized comments
+    foreach ($categories as $category => $comments) {
+        echo "Comments about $category:\n";
+        foreach ($comments as $comment) {
+            echo "<pre>- $comment</pre>\n";
+        }
+        echo "\n";
+    }
+?>
